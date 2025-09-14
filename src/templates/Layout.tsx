8747175fa2b8f453cb251b5235bc7c2180f7d092
Justin Moon <mail@justinmoon.com>
2025-09-14 10:23:03 +0100

    testerrr

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -14,7 +14,7 @@ export const Layout = ({ title, children }: LayoutProps) => (
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{title}</title>
+      <title>hetzner</title>
       {EMBEDDED_CSS ? (
         <style>{EMBEDDED_CSS}</style>
       ) : (
