c2d212d2c08fabd614d8ff894478f3da0d06be19
Justin Moon <mail@justinmoon.com>
2025-09-14 11:12:54 +0100

    Fix formatting in Layout.tsx
    Remove extra spaces around title variable

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -14,7 +14,7 @@ export const Layout = ({ title, children }: LayoutProps) => (
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{ title }</title>
+      <title>{title}</title>
       {EMBEDDED_CSS ? (
         <style>{EMBEDDED_CSS}</style>
       ) : (
