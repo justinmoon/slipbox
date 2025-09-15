97e3d95b978e342f53dfd83f052639c577784a89
Justin Moon <mail@justinmoon.com>
2025-09-15 17:07:29 +0100

    Update emoji to sparkles - deployment pipeline working!

--- scripts/manual-deploy.sh
+++ scripts/manual-deploy.sh
@@ -5,4 +5,4 @@ DIR=$(ssh justin@135.181.179.143 "mktemp -d /tmp/slipbox-XXXXXX")
 echo "🚀 Deploying to $DIR"
 
 hsync . "justin@135.181.179.143:$DIR"
-ssh justin@135.181.179.143 "cd $DIR && nix run .#ci && nix build .#slipbox && ci-deploy slipbox \$(readlink -f result)"
+ssh justin@135.181.179.143 "cd $DIR && nix build .#slipbox && ci-deploy slipbox .#slipbox"
